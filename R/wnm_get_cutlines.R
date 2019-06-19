716eb9869c6b49717ae1c6e8e9aa8c29faae2e43
Jason Timm <jaetimm@gmail.com>
2019-06-19 07:53:39 -0600

    6-19-19

--- R/wnm_get_cutlines.R
+++ R/wnm_get_cutlines.R
@@ -1,10 +1,11 @@
-#' Build/extract cutline data
+#' Extract cut line coordinates
 #'
-#' Functions extract cutting line data for usage in 'tidier' frameworks.
+#' A simple function for extracting cutting line coordinates and roll call polarity from an object of class `nomObj` via the `wnominate` package.
 #'
-#' @name get_wnominate_underlying
+#' @name wnm_get_cutlines
 #' @param x A nomObj via wnominate::wnominate output
 #' @param rollcall_obj A rollcall object from pscl package
+#' @param arrow_length A numeric value specifying arrow length for subsequent visualization
 #' @return A data frame
 
 
@@ -13,13 +14,11 @@
 #' @export
 #' @rdname wnm_get_cutlines
 wnm_get_cutlines <- function(x,
-                             dims=c(1,2),
                              arrow_length = 0.05,
-                             vote_id = NULL,
                              rollcall_obj,...) {
 
+  dims <- c(1,2)
   row.names(x$rollcalls) <- dimnames(rollcall_obj$votes)[[2]]
-
 
   constrained <- ((abs(x$rollcalls[,"spread1D"]) > 0.0 | abs(x$rollcalls[,"spread2D"]) > 0.0)
                   & (x$rollcalls[,"midpoint1D"]**2 + x$rollcalls[,"midpoint2D"]**2) < .95)
@@ -36,14 +35,14 @@ wnm_get_cutlines <- function(x,
                 weight=x$weights[dims[2]]/x$weights[dims[1]])
   names(cuts) <- ns
 
-  cuts <- data.table::rbindlist(cuts, id = 'Bill_Code') ##Rename
+  cuts <- data.table::rbindlist(cuts, id = 'Bill_Code')
 
   cuts <- cuts[complete.cases(cuts),]
-  cuts$id <- rep(1:2, length(unique(cuts$Bill_Code))) ##!
-  cuts <- data.table::melt(cuts, c('Bill_Code','id'), c('x','y'), ##!
+  cuts$id <- rep(1:2, length(unique(cuts$Bill_Code)))
+  cuts <- data.table::melt(cuts, c('Bill_Code','id'), c('x','y'),
                            variable.name="variable", value.name="value")
   cuts <- data.table::dcast (cuts,
-                             Bill_Code ~ paste0(variable,"_",id),  #SEEMS unique to nmsl53 data set -- ?
+                             Bill_Code ~ paste0(variable,"_",id),
                              value.var = "value")
   cuts <- cuts[, c('Bill_Code', 'x_1','y_1', 'x_2', 'y_2')]
 
